c8715837c9fff70713cf79a97fa931e7269e86d0
sara fathpour <sarafathpour98@gmail.com>
2020-06-24 13:13:58 +0430

    edited

--- class.php
+++ class.php
@@ -1,124 +1,188 @@
 <?php
-    class func{
-        private $conn, $tbl, $fields;
-        public function __construct($tableName, $dbName)
-        {
-            $this->setTable($tableName);
-            $this->connectDb("localhost", $dbName, "root", "1377");
+class Func
+{
+    private $conn, $tbl, $fields, $error_connecttion = null, $unique_fields = null;
+    public function __construct($tableName, $dbName, $unique = null)
+    {
+        if (isset($unique))
+            $this->setUnique($unique);
+        $this->setTable($tableName);
+        $this->connectDb("localhost", $dbName, "root", "1377");
+    }
+    public function connectDb(string $host, string $db, string $user, string $pass, string $options = null)
+    {
+        if (!isset($options)) {
+            $options = [
+                PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+            ];
         }
-        public function connectDb(string $host, string $db, string $user, string $pass, string $options=null)
-        {
-            if(!isset($options)){
-                $options = [
-                    PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
-                    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
-                ];
-            }
-            // $this->conn = config_func($host, $db, $user, $pass, $options);
-            $dsn="mysql:host=$host;dbname=$db; charset=utf8mb4";
-            set_exception_handler(function($e) {
-                error_log($e->getMessage());
-                exit('Something weird happened'); //something a user can understand
-            });
-            try{
-                $conn = new PDO($dsn, $user, $pass, $options);
-                $this->conn = $conn;
-            }catch(\PDOException $e){
-                throw new \PDOException($e->getMessage(), (int)$e->getCode());
-                die();
-            }
-        }
-        public function setTable(string $tbl)
-        {
-            $this->tbl=$tbl;
-        }
-        public function setFields(array $fields)
-        {
-            $this->fields=$fields;
-        }
-        ////////* CRUD:
-        ////* Create 
-        public function create(array $values)
-        {
-            $fields=array_keys($values);
-            foreach($fields as $field){
-                if(!in_array($field, $this->fields))
-                {
-                    die("$field not found !!");
-                    return;
-                }
-            }
-            $fields=implode(", ", $fields);
-            ////*
-            $param=array_values($values);
-            ////*
-            $num=count($values);
-            $place=str_repeat('?, ', $num-1)." ?";
-            ////*
-            $query="INSERT INTO $this->tbl ($fields) VALUES ($place)";
-            $stmt=$this->conn->prepare($query);
-            $stmt->execute($param);
-        }
-        ////* Read
-        public function readRow($cond=null)
-        {
-            if(!isset($cond))
-            {
-                $query="SELECT * FROM $this->tbl";
-                $stmt=$this->conn->prepare($query);
-                $stmt->execute();
-                $data=array();
-                while($res=$stmt->fetch())
-                    $data[]=$res;
-                if(isset($data[0])) return $data;
-                else return false;
-            }
-            else
-            {
-                $values_cond=array_values($cond);
-                $keys_cond=array_keys($cond);
-                array_walk($keys_cond, function(&$value, $key) { $value .= ' = ?'; } );
-                if(count($cond)>1)
-                    $keys_cond=implode(" AND ", $keys_cond);
-                else
-                    $keys_cond=$keys_cond[0];
-                $query="SELECT * FROM $this->tbl WHERE $keys_cond";
-                $stmt=$this->conn->prepare($query);
-                $stmt->execute($values_cond);
-            }            
-            return $stmt->fetch();
-        }    
-        
-        ////* Update
-        public function update(array $cond, array $newVal)
-        {
-            $values_newVal = array_values($newVal);
-            $keys_cond=array_keys($cond);
-            //////! append '=?' to array values
-            $fields_newVal = implode(" = ?, ",array_keys($newVal))." = ?";
-            ////!
-            array_walk($keys_cond, function(&$value, $key) { $value .= ' = ?'; } ); ////! append '=?' to array values
-            $fields_cond=count($keys_cond)>1 ? implode(" AND ", $keys_cond) : $keys_cond[0];
-            $values_cond=array_values($cond);
-            $values=array_merge($values_newVal, $values_cond); ////! merge values
-            $query="UPDATE $this->tbl SET $fields_newVal WHERE $fields_cond";
-            $stmt=$this->conn->prepare($query);
-            $stmt->execute($values);
-        }
-        ////* Delete
-        public function delete(array $cond)
-        {
-            $values_cond=array_values($cond);
-            $keys_cond=array_keys($cond);
-            array_walk($keys_cond, function(&$value, $key) { $value .= ' = ?'; } ); ////! append '=?' to array values
-            if(count($keys_cond)>1)
-                $fields_cond=implode(" AND ", $keys_cond);
-            else
-                $fields_cond=$keys_cond[0];
-            $query="DELETE FROM $this->tbl WHERE $fields_cond";
-            $stmt=$this->conn->prepare($query);
-            $stmt->execute($values_cond);
+        $dsn = "mysql:host=$host;dbname=$db; charset=utf8mb4";
+        set_exception_handler(function ($e) {
+            $this->setErrorConnection(["error-code" => 500, "error-message" => $e->getMessage()]);
+        });
+        try {
+            $conn = new PDO($dsn, $user, $pass, $options);
+            $this->conn = $conn;
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
         }
     }
-?>
+    public function setUnique($unique)
+    {
+        $this->unique_fields = $unique;
+    }
+    public function getUnique()
+    {
+        return $this->unique_fields;
+    }
+    private function setErrorConnection($error)
+    {
+        $this->error_connecttion = $error;
+    }
+    public function getErrorConnection()
+    {
+        return $this->error_connecttion;
+    }
+    public function setTable(string $tbl)
+    {
+        $this->tbl = $tbl;
+    }
+    public function setFields(array $fields)
+    {
+        $this->fields = $fields;
+    }
+    private function existUniqueField($values)
+    {
+        $cond = [];
+        foreach ($values as $key => $value) {
+            if (in_array($key, $this->unique_fields))
+                $cond[$key] = $value;
+        }
+        if (!$exist_field = $this->readRow($cond, 'OR'))
+            return false;
+        $cond_values = implode(",", $cond);
+        $cond_keys = implode(",", array_keys($cond));
+        $err_mess = "SQLSTATE[23000]: Integrity constraint violation: 1062 Duplicate entry '$cond_values' for keys '$cond_keys'";
+        $this->setErrorConnection([
+            "error-code" => 500,
+            "error-message" => $err_mess
+        ]);
+        return true;
+    }
+    ////////* CRUD:
+    ////* Create 
+    public function create(array $values)
+    {
+        // var_dump($this->unique_fields);
+        // var_dump(!$this->readRow([$this->unique_fields[0]=>$values[$this->unique_fields[0]]]));
+        $unique=$this->unique_fields;
+        if (($this->readRow([$unique[0]=>$values[$unique[0]]])))
+        {
+            $this->setErrorConnection([
+                "error-code" => 500,
+                "error-message" => "$unique[0] has already been registered"
+            ]);
+            return false;
+        }
+        if (sizeof($unique) > 0)
+            if ($this->existUniqueField($values))
+                return false;
+        $fields = array_keys($values);
+        $fields = implode(", ", $fields);
+        ////*
+        $param = array_values($values);
+        ////*
+        $num = count($values);
+        $place = str_repeat('?, ', $num - 1) . " ?";
+        ////*
+        $query = "INSERT INTO $this->tbl ($fields) VALUES ($place)";
+        $stmt = $this->conn->prepare($query);
+
+        if ($stmt->execute($param))
+            return $this->readRow(["username" => $values["username"]]);
+        else return false;
+    }
+    ////* Read
+    public function readRow($cond = null, $operator = 'AND')
+    {
+        if (!isset($cond)) {
+            $query = "SELECT userid, username, firstname, lastname FROM $this->tbl";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+        } else {
+            $values_cond = array_values($cond);
+            $keys_cond = array_keys($cond);
+            array_walk($keys_cond, function (&$value, $key) {
+                $value .= ' = ?';
+            });
+
+            $keys_cond = count($cond) > 1 ? implode($operator, $keys_cond) : $keys_cond[0]; //! $operator = OR or AND
+            $query = "SELECT userid, username, firstname, lastname FROM $this->tbl WHERE $keys_cond";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute($values_cond);
+        }
+        $data = [];
+        while ($res = $stmt->fetch())
+            $data[] = $res;
+
+        if (sizeof($data) == 0) {
+            $this->setErrorConnection([
+                "error-code" => 404,
+                "error-message" => "STATE[404]: Not found"
+            ]);
+            return false;
+        }
+        return $data;
+    }
+
+    ////* Update
+    public function update(array $cond, array $newVal)
+    {
+
+        if (!$this->readRow($cond))
+            return false;
+        if (sizeof($this->unique_fields) > 0)
+            if ($this->existUniqueField($newVal))
+                return false;
+        //
+        $values_newVal = array_values($newVal);
+        $keys_cond = array_keys($cond);
+        $values_cond = array_values($cond);
+        //////! append '=?' to array values
+        $fields_newVal = implode(" = ?, ", array_keys($newVal)) . " = ?";
+        ////!
+        array_walk($keys_cond, function (&$value, $key) {
+            $value .= ' = ?';
+        }); ////! append '=?' to array values
+        $fields_cond = count($keys_cond) > 1 ? implode(" AND ", $keys_cond) : $keys_cond[0];
+        $values = array_merge($values_newVal, $values_cond); ////! merge values
+        $query = "UPDATE $this->tbl SET $fields_newVal WHERE $fields_cond";
+        $stmt = $this->conn->prepare($query);
+        if ($stmt->execute($values))
+            return $this->readRow(["username" => $newVal["username"]]);
+        else return false;
+    }
+    ////* Delete
+    public function delete(array $cond)
+    {
+        $row = $this->readRow($cond);
+        if (!$row)
+            return false;
+        $values_cond = array_values($cond);
+        $keys_cond = array_keys($cond);
+        array_walk($keys_cond, function (&$value, $key) {
+            $value .= ' = ?';
+        }); ////! append '=?' to array values
+        if (count($keys_cond) > 1)
+            $fields_cond = implode(" AND ", $keys_cond);
+        else
+            $fields_cond = $keys_cond[0];
+        $query = "DELETE FROM $this->tbl WHERE $fields_cond";
+        $stmt = $this->conn->prepare($query);
+        if ($stmt->execute($values_cond))
+            return $row;
+        return false;
+    }
+}
--- user.php
+++ user.php
@@ -1,65 +1,95 @@
 <?php
-    require("./class.php");
-    $user=new func("users", "milogy");
-    $user->setFields(["username", "password", "firstname", "lastname"]);
-    $request = strtolower((string)$_REQUEST['REQUEST_METHOD']);
+require("./class.php");
+header('Content-Type: application/json');
+$user = new Func("users", "milogy", ["username"]);
+$state = $user->getErrorConnection();
+if (isset($state)) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    echo json_encode($state);
+}
+$user->setFields(["username", "password", "firstname", "lastname"]);
+$request = $_SERVER['REQUEST_METHOD'];
+$request = strtolower($_SERVER['REQUEST_METHOD']);
+////? Create
+if ($request == "post") {
+    $data_json = file_get_contents('php://input');
+    $data = json_decode($data_json, true);
 
-    ////? Create
-    if ($request=="post") {
-        if(!isset($_POST["username"]) || !isset($_POST["password"]) || !isset($_POST["firstname"]) || !isset($_POST["firstname"]))
-        {
-            die("Some fields are not entered");
-            exit;
-        }
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-        $firstname = $_POST["firstname"];
-        $lastname = $_POST["lastname"];
-        $user->create(["username"=>$username, "password"=>$password, "firstname"=>$firstname, "lastname"=>$lastname]);
+    if (!isset($data["username"]) || !isset($data["password"]) || !isset($data["firstname"]) || !isset($data["firstname"])) {
+        die("Some fields are not entered");
     }
-     ////? Read
-    elseif($request=="get")
-    {
-        $res=$user->readRow();
-        if($res)
-        {
-            $json_res=json_encode($res);
-            echo $json_res;
-            // var_dump($res);
-        }
+    $username = $data["username"];
+    $password = $data["password"];
+    $firstname = $data["firstname"];
+    $lastname = $data["lastname"];
+    $res = $user->create(["username" => $username, "password" => $password, "firstname" => $firstname, "lastname" => $lastname]);
+    if ($res) {
+        header("HTTP/1.0 201 Creted");
+        echo json_encode($user->readRow(["username" => $username]));
+    } else {
+        $state = $user->getErrorConnection();
+        header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+        echo json_encode($state);
+    }
+}
+////? Read
+elseif ($request == "get") {
+    $res = $user->readRow();
+    if (sizeof($res) >= 1)
+        echo json_encode($res, true);
+    else {
+        $err = $user->getErrorConnection();
+        header("HTTP/1.0 404 Not Found");
+        echo json_encode($err);
+        die();
+    }
+}
+////? Update
+elseif ($request == "put") {
+    $data_json = file_get_contents('php://input');
+    $data = json_decode($data_json, true);
+    if (
+        !isset($_GET["userid"], $data["username"]) || !isset($data["password"])
+        || !isset($data["firstname"]) || !isset($data["lastname"])
+    ) {
+        die("Some fields are not entered");
+        exit;
+    }
+    $userid = (int) $_GET["userid"];
+    $username = $data["username"];
+    $password = $data["password"];
+    $firstname = $data["firstname"];
+    $lastname = $data["lastname"];
+
+    $res = $user->update(["userid" => $userid], [
+        "username" => $username, "password" => $password,
+        "firstname" => $firstname, "lastname" => $lastname
+    ]);
+    if ($res)
+        echo json_encode($user->readRow(["username" => $username]));
+    else {
+        $err = $user->getErrorConnection();
+        if ($err["error-code"] == 404)
+            header("HTTP/1.0 404 Not Found");
         else
-            echo "Not Found";
+            header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+        echo json_encode($err);
+        die();
     }
-    ////? Update
-    elseif ($request=="put") 
-    {
-        parse_str(file_get_contents('php://input'), $data);
-        // $data=json_decode($data_json, true);
-        if(!isset($data["userid"], $data["username"]) || !isset($data["password"])
-             || !isset($data["firstname"]) || !isset($data["lastname"]))
-        {
-            die("Some fields are not entered");
-            exit;
-        }
-        $userid = $data["userid"];
-        $username = $data["username"];
-        $password = $data["password"];
-        $firstname = $data["firstname"];
-        $lastname = $data["lasttname"];
-        $user->update(["userid"=>$userid],["username"=>$username, "password"=>$password, 
-            "firstname"=>$firstname, "lastname"=>$lastname]);
+}
+////? Delete
+elseif ($request == 'delete') {
+    if (!isset($_GET["userid"])) {
+        die("Some fields are not entered");
+        exit;
     }
-    ////? Delete
-    elseif ($request=='delete')
-     {
-        parse_str(file_get_contents('php://input'), $data);
-        if(!isset($data["userid"]))
-        {
-            die("Some fields are not entered");
-            exit;
-        }
-        $userid = $data["userid"];
-
-        $user->delete(["userid"=>$userid]);
+    $userid = (int) $_GET["userid"];
+    $res = $user->delete(["userid" => $userid]);
+    if ($res) {
+        echo json_encode($res);
+    } else {
+        header("HTTP/1.0 404 Not Found");
+        echo json_encode(["Status Code" => 404, "Status Message" => "Not Found"], true);
+        die;
     }
-?>
+}
